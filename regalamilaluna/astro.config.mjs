0a0d1e7e46eef7ac597132abc400a6fd2fdbf00d
exco-consulting <lucaalbini@gmail.com>
2024-10-28 22:06:41 +0100

    Remove Astro DB

--- regalamilaluna/astro.config.mjs
+++ regalamilaluna/astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 
-import db from "@astrojs/db";
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind(), db()]
+  integrations: [tailwind()]
 });
