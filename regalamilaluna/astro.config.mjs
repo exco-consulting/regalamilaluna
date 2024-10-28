0dff7c8c10d3f8b0abdc7f93435195ac444f0ec5
exco-consulting <lucaalbini@gmail.com>
2024-10-28 21:57:16 +0100

    Revert "Merge remote-tracking branch 'refs/remotes/origin/main'"
    This reverts commit b882cfa0fb4688fe742b580cc731064862eb282b, reversing
    changes made to a414e5005b5a069029d7d833470c7ff0f6f8806a.

--- regalamilaluna/astro.config.mjs
+++ regalamilaluna/astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 
+import db from "@astrojs/db";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind()],
+  integrations: [tailwind(), db()]
 });
